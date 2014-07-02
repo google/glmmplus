bd181369c3a3b6d7ba0d955b05ee3759493d2952
Ben Ogorek <baogorek@gmail.com>
2014-07-02 11:03:25 -0700

    Adding license information to all source code files

--- tests/mice_extensions_test.R
+++ tests/mice_extensions_test.R
@@ -1,15 +1,16 @@
-#library(RUnit)
-#library(splines)
-#library(mice)
-#
-#source("../R/mice_extentions.R")
-#source("../R/analysis_functions.R");
-#source("../R/helper_functions.R");
-#source("../R/sequential.R")
-#source("../R/lmer_prediction.R")
-#source("../R/gfo_assessment.R")
-#source("../R/mice_extentions.R")
-## Setup
+# Copyright 2014 Google Inc. All rights reserved.
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 wide.df <- data.frame(pid           = 1:100,
                       my.response.1 = rnorm(100),
--- tests/statistical_fn_test.R
+++ tests/statistical_fn_test.R
@@ -1,13 +1,16 @@
-#library(mice)
-#library(lme4)
-#library(RUnit)
-#library(splines)
-#source("../R/analysis_functions.R");
-#source("../R/helper_functions.R");
-#source("../R/sequential.R")
-#source("../R/lmer_prediction.R")
-#source("../R/gfo_assessment.R")
-#source("../R/mice_extentions.R")
+# Copyright 2014 Google Inc. All rights reserved.
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 set.seed(1234)
 N <- 500
